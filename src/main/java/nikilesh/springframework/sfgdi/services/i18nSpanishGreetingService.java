6721b92f9745ac15b68beeb9b0542ebc9b56405e
NikileshRajN <nikileshnatraj@gmail.com>
2022-09-23 01:12:22 +0530

    Default service specified
    default

--- src/main/java/nikilesh/springframework/sfgdi/services/i18nSpanishGreetingService.java
+++ src/main/java/nikilesh/springframework/sfgdi/services/i18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package nikilesh.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class i18nSpanishGreetingService implements GreetingService{
 
